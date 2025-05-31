f96e24c40ec8fed504ea2e133fa0f0e14d5fe76d
10Pratik01 <using0this@gmail.com>
2025-05-31 23:30:24 +0530

    Staring with mobile view for navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import {brainwave} from "../assets"
 import {navigation} from '../constants'
+import { useLocation } from "react-router-dom"
+import Button from "./Button"
 
 const Header = () => {
+    const pathname = useLocation(); 
+
+
 
     return (
-      <div className="fixed top-0 z-50 bg-zinc-950/90 backdrop-blur-sm broder-b border-zinc-800 lg:bg-zinc-950/90 lg:backdrop-blur-sm lg:border-zinc-800">
+      <div className="fixed top-0 left-0 w-full z-50 bg-zinc-950/90 backdrop-blur-sm broder-b border-zinc-800 lg:bg-zinc-950/90 lg:backdrop-blur-sm lg:border-zinc-800">
 
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
             <a className="block w-[12rem] xl:mr-8" href="#hero">
@@ -17,13 +22,17 @@ const Header = () => {
                         <a 
                             key={item.id} 
                             href={item.url}
-                            className={`block relative text-2xl uppercase text-white transition-colors hover:text-purple-400 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold `}
+                            className={`block relative text-2xl uppercase text-white transition-colors hover:text-purple-400 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === pathname.hash ? 'z-2 lg:text-white ' : 'lg:text-white/50'} lg:leading-5 lg:hvoer:text-white xl:px-12`}
                         >
                             {item.title}
                         </a>
                     ))}
                 </div>
             </nav>
+            <a href="#signup" className="button hidden mr-8 text-white/50 transition-colors hover:text-white lg:block">New Account</a>
+            <Button className='hidden lg:flex' href='#login'>
+                    Sign in
+            </Button>
         </div>
 
       </div>
