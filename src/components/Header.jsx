8ed173e439aac9535a211aaa2f652f7287246501
10Pratik01 <using0this@gmail.com>
2025-05-31 21:55:49 +0530

    changes in header and button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@ import {brainwave} from "../assets"
 const Header = () => {
 
     return (
-      <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm broder-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm lg:border-n-6">
+      <div className="fixed top-0 z-50 bg-zinc-950/90 backdrop-blur-sm broder-b border-zinc-800 lg:bg-zinc-950/90 lg:backdrop-blur-sm lg:border-zinc-800">
 
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-            <a >
-                <img src={brainwave}  />
+            <a className="block w-[12rem] xl:mr-8" href="#hero">
+                <img src={brainwave} width={190} height={40} />
             </a>
+
+            <nav className="hidden fixed ">
+
+            </nav>
         </div>
 
       </div>
