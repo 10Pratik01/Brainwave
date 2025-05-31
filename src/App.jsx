1953cbd4a20c4b219343609511ece88b6af7b245
10Pratik01 <using0this@gmail.com>
2025-05-31 21:42:18 +0530

    started header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Button from "./components/Button";
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -9,9 +9,7 @@ const App = () => {
 
 
       <div className="pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden">
-        <Button className="mt-10 " href="#login">
-          Something
-        </Button>
+        <Header/>
       </div>
       <ButtonGradient/>
     </>
