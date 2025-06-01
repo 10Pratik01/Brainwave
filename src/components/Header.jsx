faa93e5eb06e56a04665227ad63be82a7bfc2044
10Pratik01 <using0this@gmail.com>
2025-06-01 08:12:14 +0530

    starting with Section.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import Button from "./Button"
 import Menusvg from "../assets/svg/MenuSvg"
 import {HamburgerMenu} from './design/Header'
 import { useState } from "react"
+import { disablePageScroll, enablePageScroll } from "scroll-lock"
 
 const Header = () => {
     const pathname = useLocation(); 
@@ -13,18 +14,23 @@ const Header = () => {
     const toggleNavigation = () => {
         if(openNavigation){
             setOpenNavigation(false);
+            enablePageScroll()
         }
         else{
             setOpenNavigation(true)
+            disablePageScroll()
         }
     }
 
     const handleClick = () => {
+        if(!openNavigation) return; 
+
+        enablePageScroll() 
         setOpenNavigation(false)
     }
 
     return (
-      <div className={`fixed top-0 left-0 w-full z-50 bg-zinc-950/90 backdrop-blur-sm broder-b border-zinc-800 lg:bg-zinc-950/90 lg:backdrop-blur-sm lg:border-zinc-800 ${openNavigation ? "bg-zinc-950" : "bg-zinc-950/90 backdrop-blur-sm"}`}>
+      <div className={`fixed top-0 left-0 w-full z-50 broder-b border-gray-700 lg:bg-zinc-950/90 lg:backdrop-blur-sm lg:border-zinc-800 ${openNavigation ? "bg-zinc-950" : "bg-zinc-950/90 backdrop-blur-sm"}`}>
 
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
             <a className="block w-[12rem] xl:mr-8" href="#hero">
@@ -37,15 +43,16 @@ const Header = () => {
                         <a 
                             key={item.id} 
                             href={item.url}
+                            onClick={handleClick}
                             className={`block relative text-2xl uppercase text-white transition-colors hover:text-purple-400 ${item.onlyMobile ? "lg:hidden" : ""} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${item.url === pathname.hash ? 'z-2 lg:text-white ' : 'lg:text-white/50'} lg:leading-5 lg:hvoer:text-white xl:px-12`}
                         >
                             {item.title}
                         </a>
                     ))}
-                    <HamburgerMenu/>
                 </div>
+                    <HamburgerMenu/>
             </nav>
-            <a href="#signup" className="button hidden mr-8 text-white/50 transition-colors hover:text-white lg:block">New Account</a>
+            <a href="#signup" className="text-xs font-bold uppercase tracking-wider hidden mr-8 text-white/50 transition-colors hover:text-white lg:block">New Account</a>
             <Button className='hidden lg:flex' href='#login'>
                     Sign in
             </Button>
