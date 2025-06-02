3781effd12a2f4d1d86941f7b3b13b225d4fdb43
10Pratik01 <using0this@gmail.com>
2025-06-02 18:12:02 +0530

    fixed section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,32 +1,40 @@
 import React from "react";
-import SectionSvg from '../assets/svg/SectionSvg'
+import SectionSvg from '../assets/svg/SectionSvg';
 
 const Section = ({
-    className,
-    id,
-    crosses,
-    crossesOffset,
-    customPaddings,
-    childeren 
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children  
 }) => {
+  return (
+    <div
+      id={id}
+      className={`relative ${
+        customPaddings || `py-11 lg:py-16 xl:py-40 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } ${className || ""}`}
+    >
+      {children}
 
-    return(
-        <div id={id} className={`relative ${customPaddings || `py-11 lg:py-16 xl:py-40 ${crosses ? " lg:py-32 xl:py-40 " : "" } ${className || ''} `}`}>
-            {childeren}
+     
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-zinc-900 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-zinc-900 pointer-events-none md:block lg:right-7.5 xl:right-10" />
 
-            <div className="hidden top-0 left-5 w-0.25 h-full bg-zinc-900 pointer-events-none md:block lg:left-7.5 xl:left-10 " />
-            <div className="hidden top-0 left-5 w-0.25 h-full bg-zinc-900 pointer-events-none md:block lg:right-7.5 xl:right-10 " />
+      {crosses && (
+        <>
+          
+          <div
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-gray-900 ${
+              crossesOffset || ""
+            } pointer-events-none lg:block xl:left-10 xl:right-10`}
+          />
+          <SectionSvg crossesOffset={crossesOffset} />
+        </>
+      )}
+    </div>
+  );
+};
 
-            {crosses && (
-                <>
-                    <div className={`
-                            hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-gray-900 ${crossesOffset && crossesOffset} pointer-events-none lg:block xl:left-10 xl:right-10
-                        `} />
-                        <SectionSvg crossesOffset={crossesOffset}/>
-                </>
-            )}
-        </div>
-    )
-}
-
-export default Section; 
+export default Section;
