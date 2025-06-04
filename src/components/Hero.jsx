c30d52fe8114723499c520f3ca323827feed8a47
10Pratik01 <using0this@gmail.com>
2025-06-04 16:12:23 +0530

    completed Notification.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,6 +17,7 @@ import {BackgroundCircles, BottomLine, Gradient} from "./design/Hero"
 import curve from "../assets/hero/curve.png";
 import robot from "../assets/hero/robot.jpg";
 import heroBackground from "../assets/hero/hero-background.jpg";
+import Notification from "./Notification";
 
 
 const Hero = () => {
@@ -67,7 +68,9 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 " />
+                
+                  <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 " />
+                
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-zinc-700/20 backdrop-blur-2xl border border-zinc-700/10 rounded-2xl xl:flex"> 
@@ -77,6 +80,11 @@ const Hero = () => {
                       </li>
                     ))}
                   </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                    <Notification className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex" 
+                    title="Code generation" />
                 </ScrollParallax>
               </div>
             </div>
