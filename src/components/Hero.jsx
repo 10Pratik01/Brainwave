745f586d34df0bb38255f707f67250c15594534e
10Pratik01 <using0this@gmail.com>
2025-06-04 15:36:02 +0530

    Generating.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
       <div className="max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem] relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="font-semibold text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[3.25rem] lg:leading-[4.0625rem] xl:text-[3.75rem] xl:leading-[4.5rem] mb-6">
-            Explore the Possibilites of AI chatiing with{" "}
+            Explore the Possibilites of&nbsp;AI&nbsp;chatiing with {" "}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
@@ -82,6 +82,7 @@ const Hero = () => {
           <BackgroundCircles/>
         </div>
       </div>
+      <BottomLine/>
     </Section>
   );
 };
